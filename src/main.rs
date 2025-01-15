5a84aaa553a0c5f27a79d96d76d47682dcce16e9
roopak <roopaksai5577@gmail.com>
2025-01-15 09:26:39 +0530

    Split code to multiple files

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,9 @@
-use std::io::{self, Read};
-use crossterm::terminal::{enable_raw_mode,disable_raw_mode};
-
+mod editor;
+use editor::Editor;
 
 fn main() {
-    enable_raw_mode().unwrap();
-    for b in io::stdin().bytes() {
-        let b = b.unwrap();
-        let c = b as char;
+    let editor = Editor::default();
+    editor.run();
 
-        if c.is_control() {
-            print!("Binary: {0:08b} ASCII: {0:#03} \r", b);
-        } else {
-            print!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
-        }
-        if c == 'q'{
-            disable_raw_mode().unwrap();
-            break;
 
-        }
-    }
 }
